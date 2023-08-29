712a2964a964db4f62f1f28ecf8326d6bb2453ac
Takeasoul <odenter2003@gmail.com>
2023-08-29 16:51:18 +0300

    Initial commit: Added go.mod and main.go

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
-package Task_manager
+package main
+
+import (
+	"github.com/Takeasoul/Task-manager/web"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Serve static files from "public" directory
+	e.Static("/static", "public")
+
+	// Load templates
+	e.Renderer = web.NewTemplateRenderer()
+
+	// Initialize web routes
+	web.InitRoutes(e)
+
+	// Start server
+	e.Start(":8080")
+}
